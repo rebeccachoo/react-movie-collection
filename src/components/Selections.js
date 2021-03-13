7dbc2ff0af3f767e781b345a73152c711ebc9af5
Rebecca Choo <rnwldms@gmail.com>
2021-03-13 00:05:32 -0600

    Add files via upload

---
+++ src/components/Selections.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const SelectionStyle = styled.div`
+	background-color: #ded9e2;
+	box-sizing: border-box;
+	min-height: 200px;
+	border-radius: 5px;
+`;
+
+const Selections = () => {
+	return <SelectionStyle></SelectionStyle>;
+};
+
+export default Selections;
